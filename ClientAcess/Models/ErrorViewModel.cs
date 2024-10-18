4a92176bdbf979bbe8e5ae58ccbd16e12f561913
baptista <bruno_baptista86@hotmail.com>
2024-10-18 20:59:00 +0100

    first commit

---
+++ ClientAcess/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClientAcess.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
